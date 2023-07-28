03287c9b2cab48c51401af3d6642f1df0d044f6d
wiwyil2tr <108447154+wiwyil2tr@users.noreply.github.com>
2023-07-28 19:37:37 +0800

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm ~/.themes/Windows-10 -r
+rm ~/.local/share/icons/We10X -r
+sudo rm -r /usr/share/backgrounds/wallpaper.jpg /usr/share/icons/menu.png
+echo "Uninstallation Compeleted!"
