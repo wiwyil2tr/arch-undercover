5e9098ea40969ad0a0021028fd91cafa04200d27
wiwyil2tr <108447154+wiwyil2tr@users.noreply.github.com>
2023-07-28 19:37:56 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 rm ~/.themes/Windows-10 -r
-rm ~/.local/share/icons/We10X -r
 sudo rm -r /usr/share/backgrounds/wallpaper.jpg /usr/share/icons/menu.png
 echo "Uninstallation Compeleted!"
