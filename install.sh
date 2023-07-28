721716e9a3a0240fcf133b10546cda9c720edb95
wiwyil2tr <108447154+wiwyil2tr@users.noreply.github.com>
2023-07-28 19:34:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ if [[ ! -d "$HOME/.themes" ]]; then
                 mkdir -p "$HOME/.themes"
  fi
 cp ./Windows-10 -r ~/.themes -r
-cp ./We10X We10X-dark ~/.local/share/icons -r
+cp ./windows10 ~/.local/share/icons -r
 echo "You should enter your password to install wallpaper and menu button icon"
 sudo cp ~/.themes/Windows-10/wallpaper.jpg /usr/share/backgrounds -r
 sudo cp -r ~/.themes/Windows-10/menu.png /usr/share/icons -r
